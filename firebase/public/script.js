459985f7b0fd560d257ac2ef42dba85f126e8797
UDONKONET <>
2019-01-06 00:04:31 +0900

    調整

--- firebase/public/script.js
+++ firebase/public/script.js
@@ -1,7 +1,7 @@
 function sendmail() {
-	var address, subject, body;
-	address = 'c' + 'o' + 'n' + 't' + 'a' + 'c' + 't' + '@' + 'u' + 'd' + 'o' + 'n' + 'k' + 'o' + '.' + 'n' + 'e' + 't';
-	subject = 'UDONKONETウェブサイトからのお問い合わせ';
-	body = '';
-	location.href = 'mailto:' + address + '?subject=' + subject + '&body=' + body;
+    var address, subject, body;
+    address = 'c' + 'o' + 'n' + 't' + 'a' + 'c' + 't' + '@' + 'u' + 'd' + 'o' + 'n' + 'k' + 'o' + '.' + 'n' + 'e' + 't';
+    subject = 'UDONKONETウェブサイトからのお問い合わせ';
+    body = '';
+    location.href = 'mailto:' + address + '?subject=' + subject + '&body=' + body;
 }
